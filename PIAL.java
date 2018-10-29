fde1c18041a6712eb0cac1bbb263750fa8604b5c
Reth202 <44556877+Reth202@users.noreply.github.com>
2018-10-28 21:09:28 -0500

    Update PIAL.java

--- PIAL.java
+++ PIAL.java
@@ -1,38 +1,67 @@
 package Créditos;
 public class  Corriente {
-
 }
+
+
 
 package Créditos;
-public class Especial {
-	
+public class Especial {	
 }
+
+
 
 package Créditos;
 public class Vivienda {
-
 }
+
+
 
 package Controlador;
 public abstract class Cliente {
 
-	
+public static String Nombre="";
+public static String DNI="";
+public static float Salario=0;
 }
+
+
 
 package Controlador;
 public class Docentes extends Cliente{
 
+public static String Lugar de trabajo="";
+public static String Categoría=""; //Prof. Adjunto, Asociado o Catedrático
+public static String Estado=""; //Interino o Propiedad
+	
+public Docentes (String Nombre, String DNI, float Salario);
+super(Nombre, DNI, Salario);
 }
+
+
 
 package Controlador;
-public class Addministrativos extends Cliente {
+public class Administrativos extends Cliente {
 
+public static String Sitio_Tra="";
+public static String Puesto="";
+	
+public Administrativos (String Nombre, String DNI, float Salario);
+super(Nombre, DNI, Salario);
 }
+
+
 
 package Controlador;
 public class Pensionados extends Cliente{
 
+public static int Años_Jubi=0; //Años de jubilación
+public static String Tipo de empleado=""; //Docente ó Administrativo
+
+public Pensionados (String Nombre, String DNI, float Salario);
+super(Nombre, DNI, Salario);
 }
+
+
 
 package Vista;
 public class PIAL {
